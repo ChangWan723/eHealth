710e3d71599a63da8aa5be8652405a93d71423ae
ChangWan <wan723@outlook.com>
2024-04-06 21:39:47 +0100

    optimise homepage

--- src/home/components/AppAppBar.js
+++ src/home/components/AppAppBar.js
@@ -1,25 +1,42 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
+
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
 import MenuItem from '@mui/material/MenuItem';
+import Drawer from '@mui/material/Drawer';
+import MenuIcon from '@mui/icons-material/Menu';
 import Logo from "../../layouts/full/shared/logo/Logo";
 
+const logoStyle = {
+    width: '140px',
+    height: 'auto',
+    cursor: 'pointer',
+};
+
 function AppAppBar() {
+    const [open, setOpen] = React.useState(false);
+
+    const toggleDrawer = (newOpen) => () => {
+        setOpen(newOpen);
+    };
 
     const scrollToSection = (sectionId) => {
         const sectionElement = document.getElementById(sectionId);
         const offset = 128;
         if (sectionElement) {
             const targetScroll = sectionElement.offsetTop - offset;
-            sectionElement.scrollIntoView({behavior: 'smooth'});
+            sectionElement.scrollIntoView({ behavior: 'smooth' });
             window.scrollTo({
                 top: targetScroll,
                 behavior: 'smooth',
             });
+            setOpen(false);
         }
     };
 
@@ -48,7 +65,7 @@ function AppAppBar() {
                             maxHeight: 40,
                             border: '1px solid',
                             borderColor: 'divider',
-                            boxShadow: `0 0 1px rgba(85, 166, 246, 0.1), 1px 1.5px 2px -1px rgba(85, 166, 246, 0.15), 4px 4px 12px -2.5px rgba(85, 166, 246, 0.15)`
+                            boxShadow:`0 0 1px rgba(85, 166, 246, 0.1), 1px 1.5px 2px -1px rgba(85, 166, 246, 0.15), 4px 4px 12px -2.5px rgba(85, 166, 246, 0.15)`,
                         })}
                     >
                         <Box
@@ -61,10 +78,10 @@ function AppAppBar() {
                             }}
                         >
                             <Logo/>
-                            <Box sx={{display: {xs: 'none', md: 'flex'}}}>
+                            <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
                                 <MenuItem
                                     onClick={() => scrollToSection('features')}
-                                    sx={{py: '6px', px: '12px'}}
+                                    sx={{ py: '6px', px: '12px' }}
                                 >
                                     <Typography variant="body2" color="text.primary">
                                         Features
@@ -72,7 +89,7 @@ function AppAppBar() {
                                 </MenuItem>
                                 <MenuItem
                                     onClick={() => scrollToSection('doctors')}
-                                    sx={{py: '6px', px: '12px'}}
+                                    sx={{ py: '6px', px: '12px' }}
                                 >
                                     <Typography variant="body2" color="text.primary">
                                         Doctors
@@ -80,7 +97,7 @@ function AppAppBar() {
                                 </MenuItem>
                                 <MenuItem
                                     onClick={() => scrollToSection('map')}
-                                    sx={{py: '6px', px: '12px'}}
+                                    sx={{ py: '6px', px: '12px' }}
                                 >
                                     <Typography variant="body2" color="text.primary">
                                         Pharmacies
@@ -88,7 +105,7 @@ function AppAppBar() {
                                 </MenuItem>
                                 <MenuItem
                                     onClick={() => scrollToSection('faq')}
-                                    sx={{py: '6px', px: '12px'}}
+                                    sx={{ py: '6px', px: '12px' }}
                                 >
                                     <Typography variant="body2" color="text.primary">
                                         FAQ
@@ -98,7 +115,7 @@ function AppAppBar() {
                         </Box>
                         <Box
                             sx={{
-                                display: {xs: 'none', md: 'flex'},
+                                display: { xs: 'none', md: 'flex' },
                                 gap: 0.5,
                                 alignItems: 'center',
                             }}
@@ -123,6 +140,74 @@ function AppAppBar() {
                             >
                                 Sign up
                             </Button>
+                        </Box>
+                        <Box sx={{ display: { sm: '', md: 'none' } }}>
+                            <Button
+                                variant="text"
+                                color="primary"
+                                aria-label="menu"
+                                onClick={toggleDrawer(true)}
+                                sx={{ minWidth: '30px', p: '4px' }}
+                            >
+                                <MenuIcon />
+                            </Button>
+                            <Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
+                                <Box
+                                    sx={{
+                                        minWidth: '60dvw',
+                                        p: 2,
+                                        backgroundColor: 'background.paper',
+                                        flexGrow: 1,
+                                    }}
+                                >
+                                    <Box
+                                        sx={{
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            alignItems: 'end',
+                                            flexGrow: 1,
+                                        }}
+                                    >
+                                    </Box>
+                                    <MenuItem onClick={() => scrollToSection('features')}>
+                                        Features
+                                    </MenuItem>
+                                    <MenuItem onClick={() => scrollToSection('doctors')}>
+                                        Doctors
+                                    </MenuItem>
+                                    <MenuItem onClick={() => scrollToSection('map')}>
+                                        Pharmacies
+                                    </MenuItem>
+                                    <MenuItem onClick={() => scrollToSection('faq')}>
+                                        FAQ
+                                    </MenuItem>
+                                    <Divider />
+                                    <MenuItem>
+                                        <Button
+                                            color="primary"
+                                            variant="outlined"
+                                            component="a"
+                                            href="/login"
+                                            target="_blank"
+                                            sx={{ width: '100%' }}
+                                        >
+                                            Sign in
+                                        </Button>
+                                    </MenuItem>
+                                    <MenuItem>
+                                        <Button
+                                            color="primary"
+                                            variant="contained"
+                                            component="a"
+                                            href="/register"
+                                            target="_blank"
+                                            sx={{ width: '100%' }}
+                                        >
+                                            Sign up
+                                        </Button>
+                                    </MenuItem>
+                                </Box>
+                            </Drawer>
                         </Box>
                     </Toolbar>
                 </Container>
--- src/home/components/Doctor.js
+++ src/home/components/Doctor.js
@@ -13,38 +13,38 @@ const doctorInformation = [
       {
         avatar: <Avatar alt="Emily Stanton" src="/images/doctors/emily-stanton.jpg" />,
         name: 'Dr. Emily Stanton',
-        occupation: 'Internal Medicine',
-        testimonial: "Dr. Stanton has a decade of experience in diagnosing and treating complex internal diseases. She's known for her patient-centered approach to care.",
+        field: 'Internal Medicine',
+        description: "Dr. Stanton has a decade of experience in diagnosing and treating complex internal diseases. She's known for her patient-centered approach to care.",
       },
       {
         avatar: <Avatar alt="Alex Freeman" src="/images/doctors/alex-freeman.jpg" />,
         name: 'Dr. Alex Freeman',
-        occupation: 'Surgery',
-        testimonial: "With a steady hand and precise technique, Dr. Freeman specializes in minimally invasive surgery, promoting rapid recovery and reduced patient discomfort.",
+        field: 'Surgery',
+        description: "With a steady hand and precise technique, Dr. Freeman specializes in minimally invasive surgery, promoting rapid recovery and reduced patient discomfort.",
       },
       {
         avatar: <Avatar alt="Sophia Patel" src="/images/doctors/sophia-patel.jpg" />,
         name: 'Dr. Sophia Patel',
-        occupation: 'Obstetrics and Gynecology',
-        testimonial: "Dr. Patel provides compassionate care through all stages of pregnancy and is an advocate for women's health in preventative screenings.",
+        field: 'Obstetrics and Gynecology',
+        description: "Dr. Patel provides compassionate care through all stages of pregnancy and is an advocate for women's health in preventative screenings.",
       },
       {
         avatar: <Avatar alt="Johnathan Ng" src="/images/doctors/johnathan-ng.jpg" />,
         name: 'Dr. Johnathan Ng',
-        occupation: 'Pediatrics',
-        testimonial: "Dr. Ng’s passion lies in pediatric medicine, offering a friendly and nurturing approach to his young patients and their families.",
+        field: 'Pediatrics',
+        description: "Dr. Ng’s passion lies in pediatric medicine, offering a friendly and nurturing approach to his young patients and their families.",
       },
       {
         avatar: <Avatar alt="Lisa Cho" src="/images/doctors/lisa-cho.jpg" />,
         name: 'Dr. Lisa Cho',
-        occupation: 'Internal Medicine',
-        testimonial: "With a focus on chronic disease management, Dr. Cho integrates lifestyle medicine into her practice to foster long-term wellness.",
+        field: 'Internal Medicine',
+        description: "With a focus on chronic disease management, Dr. Cho integrates lifestyle medicine into her practice to foster long-term wellness.",
       },
       {
         avatar: <Avatar alt="Omar Hassan" src="/images/doctors/omar-hassan.jpg" />,
         name: 'Dr. Omar Hassan',
-        occupation: 'Surgery',
-        testimonial: "Specializing in cardiothoracic surgery, Dr. Hassan is renowned for his work in heart and lung operations, contributing to innovative surgical techniques.",
+        field: 'Surgery',
+        description: "Specializing in cardiothoracic surgery, Dr. Hassan is renowned for his work in heart and lung operations, contributing to innovative surgical techniques.",
       },
     ];
 
@@ -83,7 +83,7 @@ export default function Doctor() {
         </Typography>
       </Box>
       <Grid container spacing={2}>
-        {doctorInformation.map((testimonial, index) => (
+        {doctorInformation.map((doctor, index) => (
           <Grid item xs={12} sm={6} md={4} key={index} sx={{ display: 'flex' }}>
             <Card
               sx={{
@@ -94,11 +94,6 @@ export default function Doctor() {
                 p: 1,
               }}
             >
-              <CardContent>
-                <Typography variant="body2" color="text.secondary">
-                  {testimonial.testimonial}
-                </Typography>
-              </CardContent>
               <Box
                 sx={{
                   display: 'flex',
@@ -108,11 +103,16 @@ export default function Doctor() {
                 }}
               >
                 <CardHeader
-                  avatar={testimonial.avatar}
-                  title={testimonial.name}
-                  subheader={testimonial.occupation}
+                  avatar={doctor.avatar}
+                  title={doctor.name}
+                  subheader={doctor.field}
                 />
               </Box>
+              <CardContent>
+                  <Typography variant="body2" color="text.secondary">
+                      {doctor.description}
+                  </Typography>
+              </CardContent>
             </Card>
           </Grid>
         ))}
